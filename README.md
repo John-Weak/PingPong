2343d92fae6bd346c734fb274eed7df41d406c6e
John-Weak <tusharbali11@gmail.com>
2022-07-19 22:47:07 +0530

    updates readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# PingPong
- PingPong written in JS with P5.js.
+# Ping Pong
+
+ <p align="center">
+<img src="https://user-images.githubusercontent.com/18065510/179807207-f3883304-f519-4d3c-b8f1-2ef4735b3b0f.gif">
+</p>
+
+Basic single sided ping pong I wrote while learning basic stuff about programming.
+
+## Motivation
+
+I wrote this to learn about core Javascript concepts like Classes.
+
+## Tech
+
+- Javascript
+- P5.js
--- src/sketch.js
+++ src/sketch.js
@@ -1,13 +1,19 @@
-var Ball1;
-var Bar1, Bar2;
-var frameRateValue = 120;
+let Ball1;
+let Bar1, Bar2;
+let frameRateValue = 120;
 function setup() {
-
-  createCanvas(800, 600);
+  createCanvas(1920, 1080);
   Ball1 = new Ball();
   Bar1 = new Bar(0, 0);
   //Bar2 = new Bar();
   frameRate(frameRateValue);
+  resizeCanvas(windowWidth, windowHeight);
+  if (windowHeight > windowWidth) {
+    factor = windowHeight;
+    factdiv = 1080;
+  } else {
+    factor = windowWidth;
+  }
 }
 
 function draw() {
@@ -17,10 +23,16 @@ function draw() {
   Ball1.move();
   Bar1.displayBar();
   Bar1.move();
-  if (Bar1.bounce(Ball1.x, Ball1.y, Ball1.diameter)) 
-    Ball1.bounce(true);
-
+  if (Bar1.bounce(Ball1.x, Ball1.y, Ball1.diameter)) Ball1.bounce(true);
 }
 
-
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  if (windowHeight > windowWidth) {
+    factor = windowHeight;
+    factdiv = 1080;
+  } else {
+    factor = windowWidth;
+    factdiv = 1920;
+  }
+}
